165a56bc434f76c4f8c44443bc8292a60ce295f2
schasins <schasins@gmail.com>
2016-04-20 12:55:24 -0700

    Cleaning up, reorganizing.

---
+++ src/scripts/content/recording_UI.js
@@ -0,0 +1,172 @@
+/**********************************************************************
+ * Author: S. Chasins
+ **********************************************************************/
+
+/**********************************************************************
+ * User event handlers
+ **********************************************************************/
+
+var RecordingHandlers = (function() { var pub = {};
+  function targetFromEvent(event){
+    var $target = $(event.target);
+    return $target.get(0);
+  }
+
+  pub.mouseoverHandler = function(event){
+    if (currentlyRecording()){
+      Tooltip.scrapingTooltip(targetFromEvent(event));
+    }
+    if (currentlyScraping()){
+      Highlight.highlight(targetFromEvent(event));
+    }
+  }
+
+  pub.mouseoutHandler = function(event){
+    if (currentlyRecording()){
+      Tooltip.removeScrapingTooltip();
+    }
+    if (currentlyScraping()){
+      Highlight.unhighlight(targetFromEvent(event));
+    }
+  }
+
+  pub.checkScrapingOn = function(event){
+    if (Scraping.scrapingCriteria(event)){ 
+      Scraping.startProcessingScrape();
+    }
+  }
+
+  pub.checkScrapingOff = function(event){
+    if (currentlyScraping() && !(Scraping.scrapingCriteria(event))){ // this is for keyup, so user is exiting the scraping mode
+      Scraping.stopProcessingScrape();
+    }
+  }
+return pub;}());
+
+document.addEventListener('mouseover', RecordingHandlers.mouseoverHandler, true);
+document.addEventListener('mouseout', RecordingHandlers.mouseoutHandler, true);
+document.addEventListener('keydown', RecordingHandlers.checkScrapingOn, true);
+document.addEventListener('keyup', RecordingHandlers.checkScrapingOff, true);
+
+/**********************************************************************
+ * For getting current status
+ **********************************************************************/
+
+function currentlyRecording(){
+  return recording == RecordState.RECORDING; // recording variable is defined in scripts/lib/record-replay/content_script.js, tells whether r+r layer currently recording
+}
+
+function currentlyScraping(){
+  return additional_recording_handlers_on.scrape;
+}
+
+/**********************************************************************
+ * Highlighting, tooltips, for giving user feedback about current node
+ **********************************************************************/
+
+var Highlight = (function() { var pub = {};
+  var highlightColorDefault = "#E04343";
+  var currNodes = [];
+  pub.highlight = function(target, highlightColor){
+    if(highlightColor === undefined) { highlightColor = highlightColorDefault;}
+    $target = $(target);
+    $target.data("stored_background_color", window.getComputedStyle(target, null).getPropertyValue('background-color'));
+    $target.data("stored_outline", window.getComputedStyle(target, null).getPropertyValue('outline'));
+    currNodes.push(target);
+    $target.css('background-color', highlightColor);
+    $target.css('outline', highlightColor+' 1px solid');
+  }
+
+  pub.unhighlight = function(target){
+    $target = $(target);
+    targetString = $target.text(); // is this actually an ok identifier?
+    $target.css('background-color', $target.data("stored_background_color"));
+    $target.css('outline', $target.data("stored_outline"));
+    var index = currNodes.indexOf(target);
+    currNodes.splice(index, 1);
+  }
+
+  pub.unhighlightRemaining = function(){
+    for (var i = 0; i < currNodes.length; i++){
+      Highlight.unhighlight(currNodes[i]);
+    }
+  }
+return pub;}());
+
+var Tooltip = (function() { var pub = {};
+  var tooltipColorDefault = "#DBDBDB";
+  var tooltipBorderColorDefault = "#B0B0B0";
+  pub.scrapingTooltip = function(node, tooltipColor, tooltipBorderColor){
+    if(tooltipColor === undefined) { tooltipColor = tooltipColorDefault;}
+    if(tooltipBorderColor === undefined) { tooltipBorderColor = tooltipBorderColorDefault;}
+    var $node = $(node);
+    var nodeText = "CTRL + ALT + click to scrape:<br>"+NodeRep.nodeToText(node);
+    var offset = $node.offset();
+    var boundingBox = node.getBoundingClientRect();
+    var newDiv = $('<div>'+nodeText+'<div/>');
+    var width = boundingBox.width;
+    if (width < 40){width = 40;}
+
+    newDiv.attr('id', 'vpbd-hightlight');
+    newDiv.css('width', width);
+    newDiv.css('top', offset.top+boundingBox.height);
+    newDiv.css('left', offset.left);
+    newDiv.css('position', 'absolute');
+    newDiv.css('z-index', 1000);
+    newDiv.css('background-color', tooltipColor);
+    newDiv.css('border', 'solid 1px '+tooltipBorderColor);
+    newDiv.css('opacity', .9);
+    $(document.body).append(newDiv);
+  }
+
+  pub.removeScrapingTooltip = function(){
+    $('#vpbd-hightlight').remove();
+  }
+return pub;}());
+
+/**********************************************************************
+ * Handle scraping interaction
+ **********************************************************************/
+
+var Scraping = (function() { var pub = {};
+  $(function(){
+    additional_recording_handlers.scrape = function(node, eventData){
+      if (eventData.type !== "click") {return null;} //only care about clicks
+      var data = NodeRep.nodeToMainpanelNodeRepresentation(node,false);
+      utilities.sendMessage("content", "mainpanel", "scrapedData", data);
+      console.log("scrape", data);
+      return data;
+    };
+  }); //run once page loaded, because else runs before r+r content script
+
+  // must keep track of current hovered node so we can highlight it when the user enters scraping mode
+  var mostRecentMousemoveTarget = null;
+  document.addEventListener('mousemove', updateMousemoveTarget, true);
+  function updateMousemoveTarget(event){
+    mostRecentMousemoveTarget = event.target;
+  }
+
+  // functions for letting the record and replay layer know whether to run the additional handler above
+  pub.startProcessingScrape = function(){
+    additional_recording_handlers_on.scrape = true;
+    Highlight.highlight(mostRecentMousemoveTarget);
+  }
+
+  pub.stopProcessingScrape = function(){
+    additional_recording_handlers_on.scrape = false;
+    Highlight.unhighlightRemaining();
+  }
+
+  // clicks during scraping mode are special.  don't want to follow links for example
+  document.addEventListener('click', scrapingClick, true);
+  function scrapingClick(event){
+    if (currentlyScraping()){
+      event.stopPropagation();
+      event.preventDefault();
+    }
+  }
+
+  pub.scrapingCriteria = function(event){
+    return event.ctrlKey && event.altKey; // convention is we need ctrl+alt+click to scrape
+  }
+return pub;}());
---
+++ src/scripts/content/setup.js
@@ -0,0 +1,74 @@
+/**********************************************************************
+ * Author: S. Chasins
+ **********************************************************************/
+
+/**********************************************************************
+ * Listeners and general set up
+ **********************************************************************/
+
+var tabID = "setme";
+utilities.listenForMessage("background", "content", "tabID", function(msg){tabID = msg; console.log("tab id: ", msg);});
+utilities.sendMessage("content", "background", "requestTabID", {});
+
+/**********************************************************************
+ * The various node representations we may need
+ **********************************************************************/
+
+var NodeRep = (function() { var pub = {};
+	pub.nodeToMainpanelNodeRepresentation = function(node){
+	  if (node === null){
+	    return {text: "", xpath: "", frame: SimpleRecord.getFrameId()};
+	  }
+	  return {text: NodeRep.nodeToText(node), xpath: nodeToXPath(node), frame: SimpleRecord.getFrameId()};
+	}
+
+	pub.nodeToText = function(node){
+	  //var text = node.innerText;
+	  return getElementText(node);
+	}
+
+	function getElementText(el){
+	  var text = getElementTextHelper(el);
+	  if (text == null || text == undefined || text == ""){ // should empty text also be null?
+	    return null;
+	  }
+	  text = text.trim();
+	  return text;
+	}
+
+	function getElementTextHelper(el) {
+	    var text = '';
+	    // Text node (3) or CDATA node (4) - return its text
+	    if ( (el.nodeType === 3) || (el.nodeType === 4) ) {
+	        return el.nodeValue.trim();
+	    // If node is an element (1) and an img, input[type=image], or area element, return its alt text
+	    }
+	    else if ( (el.nodeType === 1) && (
+	            (el.tagName.toLowerCase() == 'img') ||
+	            (el.tagName.toLowerCase() == 'area') ||
+	            ((el.tagName.toLowerCase() == 'input') && el.getAttribute('type') && (el.getAttribute('type').toLowerCase() == 'image'))
+	            ) ) {
+	        altText = el.getAttribute('alt')
+	        if (altText == null || altText == undefined){
+	          altText = ''
+	        }
+	        return altText.trim();
+	        return el.getAttribute('alt').trim() || '';
+	    }
+	    // Traverse children unless this is a script or style element
+	    else if ( (el.nodeType === 1) && !el.tagName.match(/^(script|style)$/i)) {
+	        var text = "";
+	        var children = el.childNodes;
+	        for (var i = 0, l = children.length; i < l; i++) {
+	            var newText = getElementText(children[i]);
+	            if (newText == null || newText == undefined){
+	              newText = "";
+	            }
+	            if (newText.length > 0){
+	              text+=newText+"\n";
+	            }
+	        }
+	        return text;
+	    }
+	}
+return pub;}());

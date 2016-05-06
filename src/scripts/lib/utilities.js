011594092f94bce6ddbc4b5b38c2bdd3a361c887
schasins <schasins@gmail.com>
2016-05-06 09:56:19 -0700

    Adding to our utilities so that it's easy to attach a temporary listener that will fire only once so that we can -- if we know when to expect a response -- send our response to exactly the context we want, with custom callback.

--- src/scripts/lib/utilities.js
+++ src/scripts/lib/utilities.js
@@ -1,91 +1,131 @@
-var utilities = {};
+var utilities = (function() { var pub = {};
 
-utilities.listenForMessage = function(from, to, subject, fn){
-  console.log("Listening for messages: "+ from+" : "+to+" : "+subject);
-  if (to === "background" || to === "mainpanel"){
-    chrome.runtime.onMessage.addListener(function(msg, sender) {
-      if (msg.from && (msg.from === from) && 
-        msg.subject && (msg.subject === subject)) {
-        msg.content.tab_id = sender.tab.id;
-        console.log("Receiving message: ", msg);
-        console.log("from tab id: ", sender.tab.id);
-      fn(msg.content);
+  var listenerCounter = 0;
+  var runtimeListeners = {};
+  var extensionListeners = {};
+
+  chrome.runtime.onMessage.addListener(function(msg, sender) {
+    for (var key in runtimeListeners){
+      runtimeListeners[key](msg, sender);
     }
   });
-  }
-  else if (to === "content"){
-    chrome.extension.onMessage.addListener(function(msg, sender) {
-      var frame_id = SimpleRecord.getFrameId();
-      if (msg.frame_ids_include && msg.frame_ids_include.indexOf(frame_id) < -1){
-        console.log("Msg for frames with ids "+msg.frame_ids_include+", but this frame has id "+frame_id+".");
-        return;
-      }
-      if (msg.frame_ids_exclude && msg.frame_ids_exclude.indexOf(frame_id) > -1){
-        console.log("Msg for frames without ids "+msg.frame_ids_exclude+", but this frame has id "+frame_id+".");
-        return;
-      }
-      if (msg.from && (msg.from === from) && 
-        msg.subject && (msg.subject === subject)) {
-        console.log("Receiving message: ", msg);
-      fn(msg.content);
+
+  chrome.extension.onMessage.addListener(function(msg, sender) {
+    for (var key in extensionListeners){
+      extensionListeners[key](msg, sender);
     }
   });
-  }
-};
 
-utilities.sendMessage = function(from, to, subject, content, frame_ids_include, frame_ids_exclude, tab_ids_include, tab_ids_exclude){
-  if ((from ==="background" || from ==="mainpanel") && to === "content"){
-    var msg = {from: from, subject: subject, content: content, frame_ids_include: frame_ids_include, frame_ids_exclude: frame_ids_exclude};
-    console.log("Sending message: ", msg);
-    console.log(tab_ids_include, tab_ids_exclude);
-    if (tab_ids_include){
-      for (var i =0; i<tab_ids_include.length; i++){
-        chrome.tabs.sendMessage(tab_ids_include[i], msg); 
-      } 
-      console.log("(Sent to ", tab_ids_include.length, " tabs: ", tab_ids_include, " )");
+  pub.listenForMessage = function(from, to, subject, fn, key){
+    if (key === undefined){ key = listenerCounter; }
+    console.log("Listening for messages: "+ from+" : "+to+" : "+subject);
+    listenerCounter += 1;
+    if (to === "background" || to === "mainpanel"){
+      runtimeListeners[key] = function(msg, sender) {
+        if (msg.from && (msg.from === from) && msg.subject && (msg.subject === subject)) {
+          msg.content.tab_id = sender.tab.id;
+          console.log("Receiving message: ", msg);
+          console.log("from tab id: ", sender.tab.id);
+          fn(msg.content);
+          return true;
+        }
+        return false;
+      };
     }
-    else{
-        chrome.tabs.query({windowType: "normal"}, function(tabs){
-          tabs_messaged = 0;
-          for (var i =0; i<tabs.length; i++){
-            if (!(tab_ids_exclude && tab_ids_exclude.indexOf(tabs[i].id) > -1)){
-              chrome.tabs.sendMessage(tabs[i].id, msg); 
-              tabs_messaged ++;
+    else if (to === "content"){
+      extensionListeners[key] = function(msg, sender) {
+        var frame_id = SimpleRecord.getFrameId();
+        if (msg.frame_ids_include && msg.frame_ids_include.indexOf(frame_id) < -1){
+          console.log("Msg for frames with ids "+msg.frame_ids_include+", but this frame has id "+frame_id+".");
+          return false;
+        }
+        if (msg.frame_ids_exclude && msg.frame_ids_exclude.indexOf(frame_id) > -1){
+          console.log("Msg for frames without ids "+msg.frame_ids_exclude+", but this frame has id "+frame_id+".");
+          return false;
+        }
+        if (msg.from && (msg.from === from) && msg.subject && (msg.subject === subject)) {
+          console.log("Receiving message: ", msg);
+          fn(msg.content);
+          return true;
+        }
+        return false;
+      };
+    }
+  };
+
+  var oneOffListenerCounter = 0;
+
+  pub.listenForMessageOnce = function(from, to, subject, fn){
+    console.log("Listening once for message: "+ from+" : "+to+" : "+subject);
+    var key = oneOffListenerCounter;
+    var newfunc = null;
+    oneOffListenerCounter += 1;
+    if (to === "background" || to === "mainpanel"){
+      newfunc = function(){fn(); delete runtimeListeners[key];};
+    }
+    else if (to === "content"){
+      newfunc = function(){fn(); delete extensionListeners[key];};
+    }
+    pub.listenForMessage(from, to, subject, newfunc, key);
+  }
+
+  pub.sendMessage = function(from, to, subject, content, frame_ids_include, frame_ids_exclude, tab_ids_include, tab_ids_exclude){
+    if ((from ==="background" || from ==="mainpanel") && to === "content"){
+      var msg = {from: from, subject: subject, content: content, frame_ids_include: frame_ids_include, frame_ids_exclude: frame_ids_exclude};
+      console.log("Sending message: ", msg);
+      console.log(tab_ids_include, tab_ids_exclude);
+      if (tab_ids_include){
+        for (var i =0; i<tab_ids_include.length; i++){
+          chrome.tabs.sendMessage(tab_ids_include[i], msg); 
+        } 
+        console.log("(Sent to ", tab_ids_include.length, " tabs: ", tab_ids_include, " )");
+      }
+      else{
+          chrome.tabs.query({windowType: "normal"}, function(tabs){
+            tabs_messaged = 0;
+            for (var i =0; i<tabs.length; i++){
+              if (!(tab_ids_exclude && tab_ids_exclude.indexOf(tabs[i].id) > -1)){
+                chrome.tabs.sendMessage(tabs[i].id, msg); 
+                tabs_messaged ++;
+              }
             }
-          }
-          console.log("(Sent to "+tabs_messaged+" tabs.)");
-      });
+            console.log("(Sent to "+tabs_messaged+" tabs.)");
+        });
+      }
     }
-  }
-  else if (from === "content") {
-    var msg = {from: "content", subject: subject, content: content};
-    console.log("Sending message: ", msg);
-    chrome.runtime.sendMessage(msg);
-  }
-};
-
-var DOMCreationUtilities = {};
-
-DOMCreationUtilities.replaceContent = function(div1, div2){
-  div1.html(div2.html());
-};
-
-DOMCreationUtilities.arrayOfTextsToTableRow = function(array){
-    var $tr = $("<tr></tr>");
-    for (var j= 0; j< array.length; j++){
-      var $td = $("<td></td>");
-      $td.html(_.escape(array[j]).replace(/\n/g,"<br>"));
-      $tr.append($td);
+    else if (from === "content") {
+      var msg = {from: "content", subject: subject, content: content};
+      console.log("Sending message: ", msg);
+      chrome.runtime.sendMessage(msg);
     }
-    return $tr;
-  }
+  };
 
-DOMCreationUtilities.arrayOfArraysToTable = function(arrayOfArrays){
-    var $table = $("<table></table>");
-    for (var i = 0; i< arrayOfArrays.length; i++){
-      var array = arrayOfArrays[i];
-      $tr = DOMCreationUtilities.arrayOfTextsToTableRow(array);
-      $table.append($tr);
+return pub; }());
+
+var DOMCreationUtilities = (function() { var pub = {};
+
+  pub.replaceContent = function(div1, div2){
+    div1.html(div2.html());
+  };
+
+  pub.arrayOfTextsToTableRow = function(array){
+      var $tr = $("<tr></tr>");
+      for (var j= 0; j< array.length; j++){
+        var $td = $("<td></td>");
+        $td.html(_.escape(array[j]).replace(/\n/g,"<br>"));
+        $tr.append($td);
+      }
+      return $tr;
     }
-    return $table;
-  }
+
+  pub.arrayOfArraysToTable = function(arrayOfArrays){
+      var $table = $("<table></table>");
+      for (var i = 0; i< arrayOfArrays.length; i++){
+        var array = arrayOfArrays[i];
+        $tr = DOMCreationUtilities.arrayOfTextsToTableRow(array);
+        $table.append($tr);
+      }
+      return $table;
+    }
+
+return pub; }());

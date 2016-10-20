a151280721ccddad3e29599482abbefe96323947
schasins <schasins@gmail.com>
2016-10-20 14:49:23 -0700

    Dealing with the way the extension fails in new versions of Chrome because of trying to request data from our server (over http) when modern Chrome won't allow http requests from within https pages.

--- src/scripts/content/recording_UI.js
+++ src/scripts/content/recording_UI.js
@@ -289,7 +289,10 @@ var RelationPreview = (function() { var pub = {};
   var knownRelations = [];
   function setup(){
     console.log("running setup");
-    $.post('http://kaofang.cs.berkeley.edu:8080/allpagerelations', { url: window.location.href }, function(resp){ 
+    // have to use postForMe right now to make the extension do the acutal post request, because modern Chrome won't let us
+    // requrest http content from https pages and we don't currently have ssl certificate for kaofang
+    utilities.sendMessage("content", "background", "postForMe", {url: 'http://kaofang.cs.berkeley.edu:8080/allpagerelations', params: { url: window.location.href }});
+    utilities.listenForMessageOnce("content", "background", "postForMe", function(resp){
       console.log(resp);
       knownRelations = resp.relations;
       preprocessKnownRelations();

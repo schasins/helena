5d33f15f4b7399cc2c81eb08da84e1a301f18188
schasins <schasins@gmail.com>
2017-03-23 14:55:12 -0700

    The selenium script should now be able to run the whole first experiment, iterating through a set of programs and running them simultaneously in entityScope and entityScope ignorning mode, reporting the ids of the datasets that result.

--- test/seleniumBasic.py
+++ test/seleniumBasic.py
@@ -42,12 +42,39 @@ def runEntityScopeAndNoEntityScopeVersionsInParallel(programId):
 	runScrapingProgram(programId, "")
 	runScrapingProgram(programId, "{ignoreEntityScope: true}")	
 
-def main():
-	runEntityScopeAndNoEntityScopeVersionsInParallel(45)
+def blockingRepeatUntilNonFalseAnswer(lam):
+	ans = lam()
+	print ans
+	if (not ans):
+		time.sleep(1)
+		return blockingRepeatUntilNonFalseAnswer(lam)
+	else:
+		return ans
 
+def getDatasetIdForDriver(driver):
+	getDatasetId = lambda : driver.execute_script("if (datasetsBeingScrapedNow.length > 0) {return datasetsBeingScrapedNow[0];} else { return false;}")
+	return blockingRepeatUntilNonFalseAnswer(getDatasetId)
+
+def getWhetherDone(driver):
+	getHowManyDone = lambda: driver.execute_script("if (scrapingRunsCompleted === 0) {return false;} else {return scrapingRunsCompleted}")
+	return blockingRepeatUntilNonFalseAnswer(getHowManyDone)
+
+def entityScopeVsNoEntityScopeFirstRunExperiment(programIdsLs):
+	global drivers
+	for programId in programIdsLs:
+		runEntityScopeAndNoEntityScopeVersionsInParallel(programId)
+		datasetIds = []
+		for driver in drivers:
+			datasetIds.append(getDatasetIdForDriver(driver))
+		print datasetIds
+		for driver in drivers:
+			getWhetherDone(driver)
+			# note that we'll only get out of this loop once all drivers have finished the scripts they're executing
+		for driver in drivers:
+			driver.close()
+		drivers = []
+
+def main():
+	entityScopeVsNoEntityScopeFirstRunExperiment([60,45])
 
 main()
-a = raw_input("Type 'done' to close Chrome window...")
-if (a == 'done'):
-	for driver in drivers:
-		driver.close()

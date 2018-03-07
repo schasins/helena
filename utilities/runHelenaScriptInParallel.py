474761f6892ce3c8f4e944adc44ecfb8a03deb7a
schasins <schasins@gmail.com>
2018-03-07 15:55:59 -0800

    Turning off logging the JS console log unless debug is on in the runParallel script.

--- utilities/runHelenaScriptInParallel.py
+++ utilities/runHelenaScriptInParallel.py
@@ -27,6 +27,8 @@ timeoutInHours = float(sys.argv[3])
 howManyRunsToAllowPerWorker = int(sys.argv[4])
 chromeDriverPath = sys.argv[5]
 
+debug = False
+
 from pyvirtualdisplay import Display
 display = Display(visible=0, size=(800, 800))  
 display.start()
@@ -40,8 +42,8 @@ def newDriver(profile):
 	# chrome_options.add_argument("user-data-dir=profiles/" + profile)
 	# chrome_options.add_argument("--display=:0") 
 
-        desired = DesiredCapabilities.CHROME
-        desired ['loggingPrefs'] = { 'browser':'ALL' }
+	desired = DesiredCapabilities.CHROME
+	desired ['loggingPrefs'] = { 'browser':'ALL' }
 
 	driver = webdriver.Chrome(chromeDriverPath, chrome_options=chrome_options, desired_capabilities=desired)
 
@@ -68,15 +70,16 @@ def newDriver(profile):
 def runScrapingProgramHelper(driver, progId, optionsStr):
 	driver.execute_script("RecorderUI.loadSavedProgram(" + str(progId) + ");")
 
-        time.sleep(10)
-        data = driver.get_log('browser')
-        for line in data:
-                print line
+	if debug:
+		time.sleep(10)
+		data = driver.get_log('browser')
+		for line in data:
+				print line
 
 	runCurrentProgramJS = """
 	function repeatUntilReadyToRun(){
 		console.log("repeatUntilReadyToRun");
-        // ringerUseXpathFastMode = true; // just for the peru one.  remove this later
+		// ringerUseXpathFastMode = true; // just for the peru one.  remove this later
 		if (!RecorderUI.currentHelenaProgram){
 			setTimeout(repeatUntilReadyToRun, 100);
 		}
@@ -92,12 +95,13 @@ def runScrapingProgramHelper(driver, progId, optionsStr):
 def blockingRepeatUntilNonFalseAnswer(lam, driver):
 	ans = lam()
 	while (not ans):
-                time.sleep(5)
+		time.sleep(5)
 		ans = lam()
-                data = driver.get_log('browser')
-                print "log so far"
-                for line in data:
-                        print line
+		if debug:
+			data = driver.get_log('browser')
+			print "log so far"
+			for line in data:
+					print line
 	return ans
 
 def getDatasetIdForDriver(driver):
@@ -110,60 +114,60 @@ def getWhetherDone(driver):
 
 class RunProgramProcess(Process):
 
-        def __init__(self, profile, programId, optionStr, numTriesSoFar=0):
-                super(RunProgramProcess,self).__init__()
+		def __init__(self, profile, programId, optionStr, numTriesSoFar=0):
+				super(RunProgramProcess,self).__init__()
 
-                self.profile = profile
-                self.programId = programId
-                self.optionStr = optionStr
-                self.numTriesSoFar = numTriesSoFar
-		self.driver = newDriver(self.profile)
+				self.profile = profile
+				self.programId = programId
+				self.optionStr = optionStr
+				self.numTriesSoFar = numTriesSoFar
+				self.driver = newDriver(self.profile)
 
-        def run(self):
-                self.runInternals()
+		def run(self):
+				self.runInternals()
 
-        def runInternals(self):
-                try:
-                    runScrapingProgramHelper(self.driver, self.programId, self.optionStr)
-                    done = getWhetherDone(self.driver)
-                    self.driver.close()
-                    self.driver.quit()
-                except Exception as e:
-                        # assume we can just recover by trying again
-                        if (self.numTriesSoFar < 3):
-                                self.numTriesSoFar += 1
-                                self.runInternals()
-                        else:
-                                logging.error(traceback.format_exc())
+		def runInternals(self):
+				try:
+					runScrapingProgramHelper(self.driver, self.programId, self.optionStr)
+					done = getWhetherDone(self.driver)
+					self.driver.close()
+					self.driver.quit()
+				except Exception as e:
+						# assume we can just recover by trying again
+						if (self.numTriesSoFar < 3):
+								self.numTriesSoFar += 1
+								self.runInternals()
+						else:
+								logging.error(traceback.format_exc())
 
-        def terminate(self):
-		try:
-		    if (self.driver):
-			    self.driver.close()
-			    self.driver.quit()
-		except: # catch *all* exceptions
-		    print "tried to close driver but no luck. probably already closed"
-                super(RunProgramProcess, self).terminate()
+		def terminate(self):
+			try:
+				if (self.driver):
+					self.driver.close()
+					self.driver.quit()
+			except: # catch *all* exceptions
+				print "tried to close driver but no luck. probably already closed"
+				super(RunProgramProcess, self).terminate()
 
 
 def joinProcesses(procs, timeoutInSeconds):
-        pnum = len(procs)
-        bool_list = [True]*pnum
-        start = time.time()
-        while time.time() - start <= timeoutInSeconds:
-                for i in range(pnum):
-                        bool_list[i] = procs[i].is_alive()
-                if np.any(bool_list):
-                        time.sleep(5)
-                else:
-                        print "time to finish: ", time.time() - start
-                        return True
-        else:
-                print "timed out, killing all processes", time.time() - start
-                for p in procs:
-                        p.terminate()
-                        p.join()
-                return False
+	pnum = len(procs)
+	bool_list = [True]*pnum
+	start = time.time()
+	while time.time() - start <= timeoutInSeconds:
+		for i in range(pnum):
+				bool_list[i] = procs[i].is_alive()
+		if np.any(bool_list):
+				time.sleep(5)
+		else:
+				print "time to finish: ", time.time() - start
+				return True
+	else:
+		print "timed out, killing all processes", time.time() - start
+		for p in procs:
+				p.terminate()
+				p.join()
+		return False
    
 
 def oneRun(programId, threadCount, timeoutInSeconds, mode):
@@ -195,9 +199,9 @@ def oneRun(programId, threadCount, timeoutInSeconds, mode):
 		
 		# below will be true if all complete within the time limit, else false
 		noErrorsRunComplete = joinProcesses(procs, timeoutInSeconds)
-                return # can get rid of this if I remove the while loop above.  that was for recovery by restart stuff, which we don't want here.  don't want to run forever
+		return # can get rid of this if I remove the while loop above.  that was for recovery by restart stuff, which we don't want here.  don't want to run forever
 
 def main():
-        oneRun([scriptName], numParallelBrowsers, int(timeoutInHours * 60 * 60), "lockBased")
+		oneRun([scriptName], numParallelBrowsers, int(timeoutInHours * 60 * 60), "lockBased")
 
 main()

5fb9e0dedbb69164440a6d43774f8cb34c037a2d
schasins <schasins@gmail.com>
2018-03-07 22:08:37 -0800

    Just using ssh to connect to the machines.

--- utilities/runHelenaScriptDistributed.py
+++ utilities/runHelenaScriptDistributed.py
@@ -1,28 +1,27 @@
+import paramiko
 import boto3
-import botocore
 
-def execute_commands_on_linux_instances(client, commands, tag):
-    """Runs commands on remote linux instances
-    :param client: a boto/boto3 ssm client
-    :param commands: a list of strings, each one a command to execute on the instances
-    :param instance_ids: a list of instance_id strings, of the instances on which to execute the command
-    :return: the response from the send_command function (check the boto3 docs for ssm client.send_command() )
-    """
-
-    resp = client.send_command(
-        DocumentName="AWS-RunShellScript", # One of AWS' preconfigured documents
-        Parameters={'commands': commands},
-        Targets=[
-            {
-                'Key': "tag:" + tag,
-                'Values': ["true"]
-            }
-        ]
-    )
-    return resp
-
-client = boto3.client('ssm') # Need your credentials here
-commands = ['echo "hello world"']
 tag = "helena-1"
-resp = execute_commands_on_linux_instances(client, commands, tag)
-print resp
+
+ec2 = boto3.client('ec2')  
+tags = [{  
+    'Name': 'tag:' + tag,
+    'Values': ['true']
+    }]
+reservations = ec2.describe_instances(Filters=tags)
+ip = reservations["Reservations"][0]["Instances"][0]["PublicIpAddress"]
+
+k = paramiko.RSAKey.from_private_key_file("/Users/schasins/.ssh/MyKeyPair.pem")
+c = paramiko.SSHClient()
+c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+print "connecting"
+c.connect( hostname = ip, username = "ec2-user", pkey = k )
+print "connected"
+commands = ['(cd helena/utilities; python runHelenaScriptInParallel.py 1022 1 1 1 ./chromedriver)']
+for command in commands:
+    print "Executing {}".format( command )
+    stdin , stdout, stderr = c.exec_command(command)
+    print stdout.read()
+    print( "Errors")
+    print stderr.read()
+c.close()

02ccca6375efcfea7a39e6fb123bcb24fbc63626
schasins <schasins@gmail.com>
2018-04-08 17:35:04 -0700

    Now taking better advantage of the machines we have running at once.

--- utilities/runHelenaScriptDistributed.py
+++ utilities/runHelenaScriptDistributed.py
@@ -24,17 +24,9 @@ reservations = ec2.describe_instances(Filters=tags)
 pp = pprint.PrettyPrinter(indent=1)
 pp.pprint(reservations)
 
-# ok, first get an id for the new dataset run
-# before we can do anything else, we need to get the dataset id that we'll use for all of the 'threads'
-# 'http://kaofang.cs.berkeley.edu:8080/newprogramrun', {name: dataset.name, program_id: dataset.program_id}
-r = requests.post('http://kaofang.cs.berkeley.edu:8080/newprogramrun', data = {"name": str(scriptName)+"_"+str(numDistributedMachines)+"_distributed_hashBased", "program_id": scriptName})
-output = r.json()
-runid = output["run_id"]
-print "current parallel run's dataset id:", runid
-print "-----"
-
 # what machines do we have available to us?  let's get their ips
 availableIps = []
+runIdsForThisStage = []
 reservationsDeets = reservations["Reservations"]
 l = len(reservationsDeets)
 for i in range(l):
@@ -58,7 +50,7 @@ def talkToOneDistributedMachine(i):
 	print "connected"
 	# below, 1 is fixed because for now we only set one browser instance going on any given distributed machine
 	# also all our amazon images have chromedriver in the same folder where we run, thus the hardcoded chromedriver loc
-	com = "python runHelenaScriptInParallel.py " + str(scriptName) + " 1 " + str(timeoutInHours) + " " + str(howManyRunsToAllowPerWorker) + " ./chromedriver " + str(runid) + ")"
+	com = "python runHelenaScriptInParallel.py " + str(scriptName) + " 1 " + str(timeoutInHours) + " " + str(howManyRunsToAllowPerWorker) + " ./chromedriver " + str(runIdsForThisStage[i]) + ")"
 	commands = ['(cd helena/utilities;' + com]
 	for command in commands:
 	    print "Executing {}".format( command )
@@ -69,5 +61,30 @@ def talkToOneDistributedMachine(i):
 	c.close()
 
 # all right, now we know all the ips available to us.  let's give some out
-pool = multiprocessing.Pool(numDistributedMachines)
-pool.map(talkToOneDistributedMachine, range(0, numDistributedMachines))
+#pool = multiprocessing.Pool(numDistributedMachines)
+#pool.map(talkToOneDistributedMachine, range(0, numDistributedMachines))
+
+#stages = [[8], [6], [1,2,4]]
+stages = [[2], [1,1]]
+for stage in stages:
+	runIdsForThisStage = []
+	print "-----"
+	for numMachines in stage:
+		# make a new id for this run of numMachines machines
+		# before we can do anything else, we need to get the dataset id that we'll use for all of the 'threads'
+		# 'http://kaofang.cs.berkeley.edu:8080/newprogramrun', {name: dataset.name, program_id: dataset.program_id}
+		r = requests.post('http://kaofang.cs.berkeley.edu:8080/newprogramrun', data = {"name": str(scriptName)+"_"+str(numMachines)+"_distributed_hashBased", "program_id": scriptName})
+		output = r.json()
+		runid = output["run_id"]
+		print "generating run's dataset id:", runid, numMachines
+		for i in range(numMachines):
+			runIdsForThisStage.append(runid)
+	if (len(runIdsForThisStage) > len(availableIps)):
+		print "Woah, tried to do a stage that has more machines running in parallel than we have machines.  Fix."
+		exit(1)
+	# ok, we have all the ips we want and we know which set of machines is going to be working on which runs
+	numMachinesWeActuallyWant = len(runIdsForThisStage)
+	pool = multiprocessing.Pool(numMachinesWeActuallyWant)
+	pool.map(talkToOneDistributedMachine, range(0, numMachinesWeActuallyWant))
+
+

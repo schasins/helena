e9d85917090f4e41bca66fbd8a0647d2b1841266
schasins <schasins@gmail.com>
2018-04-09 00:17:06 -0700

    Set up for distributed experiements.

--- utilities/runHelenaScriptDistributed.py
+++ utilities/runHelenaScriptDistributed.py
@@ -13,6 +13,8 @@ numDistributedMachines = int(sys.argv[2])
 timeoutInHours = float(sys.argv[3])
 howManyRunsToAllowPerWorker = int(sys.argv[4])
 
+debug = False
+
 tag = "helena-1"
 
 ec2 = boto3.client('ec2', region_name='us-west-2')  
@@ -30,24 +32,27 @@ runIdsForThisStage = []
 reservationsDeets = reservations["Reservations"]
 l = len(reservationsDeets)
 for i in range(l):
-	machine = reservationsDeets[i]["Instances"][0]
-	if (not "PublicIpAddress" in machine):
-		# this one doesn't have a public ip address, probably because it's not running right now
-		continue
-	ip = machine["PublicIpAddress"]
-	availableIps.append(ip)
+	instances = reservationsDeets[i]["Instances"]
+	for j in range(len(instances)):
+		if (not "PublicIpAddress" in instances[j]):
+			# this one doesn't have a public ip address, probably because it's not running right now
+			continue
+		ip = instances[j]["PublicIpAddress"]
+		availableIps.append(ip)
+
+if debug: print availableIps
 
 # the function that will actually talk to a given machine at a given index in the list of ips
 def talkToOneDistributedMachine(i):
 	ip = availableIps[i]
-	print "ip", ip
+	if debug: print "ip", ip
 	#k = paramiko.RSAKey.from_private_key_file("/Users/schasins/.ssh/MyKeyPair.pem")
 	k = paramiko.RSAKey.from_private_key_file("/Users/sarahchasins/.ssh/MyKeyPair.pem")
 	c = paramiko.SSHClient()
 	c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	print "connecting"
+	if debug: print "connecting"
 	c.connect( hostname = ip, username = "ec2-user", pkey = k )
-	print "connected"
+	if debug: print "connected"
 	# below, 1 is fixed because for now we only set one browser instance going on any given distributed machine
 	# also all our amazon images have chromedriver in the same folder where we run, thus the hardcoded chromedriver loc
 	com = "python runHelenaScriptInParallel.py " + str(scriptName) + " 1 " + str(timeoutInHours) + " " + str(howManyRunsToAllowPerWorker) + " ./chromedriver " + str(runIdsForThisStage[i]) + ")"
@@ -59,16 +64,17 @@ def talkToOneDistributedMachine(i):
 	    print( "Errors")
 	    print stderr.read()
 	c.close()
+	print "finished one thread for: ", runIdsForThisStage[i]
 
 # all right, now we know all the ips available to us.  let's give some out
 #pool = multiprocessing.Pool(numDistributedMachines)
 #pool.map(talkToOneDistributedMachine, range(0, numDistributedMachines))
 
-#stages = [[8], [6], [1,2,4]]
-stages = [[2], [1,1]]
+stages = [[8], [6], [1,2,4]]
+#stages = [[3], [2,1]]
 for stage in stages:
 	runIdsForThisStage = []
-	print "-----"
+	if debug: print "-----"
 	for numMachines in stage:
 		# make a new id for this run of numMachines machines
 		# before we can do anything else, we need to get the dataset id that we'll use for all of the 'threads'
@@ -76,7 +82,7 @@ for stage in stages:
 		r = requests.post('http://kaofang.cs.berkeley.edu:8080/newprogramrun', data = {"name": str(scriptName)+"_"+str(numMachines)+"_distributed_hashBased", "program_id": scriptName})
 		output = r.json()
 		runid = output["run_id"]
-		print "generating run's dataset id:", runid, numMachines
+		if debug: print "generating run's dataset id:", runid, numMachines
 		for i in range(numMachines):
 			runIdsForThisStage.append(runid)
 	if (len(runIdsForThisStage) > len(availableIps)):
@@ -86,5 +92,9 @@ for stage in stages:
 	numMachinesWeActuallyWant = len(runIdsForThisStage)
 	pool = multiprocessing.Pool(numMachinesWeActuallyWant)
 	pool.map(talkToOneDistributedMachine, range(0, numMachinesWeActuallyWant))
+	with open("runids.txt", "a") as myfile:
+		for id in list(set(runIdsForThisStage)):
+			myfile.write(str(id) + "\n")
+
 
 
